7dab57b6d819a6f6ce9c053fd289b6b89762e576
IAmRGroot <rorogro@gmail.com>
2021-02-06 13:17:11 +0000

    Fix

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def ping():
 
         if days > 0.25:
             notify(message)
-            db.update(({'time': time.time()}, where('action') == 'last_notified'))
+            db.update({'time': time.time()}, where('action') == 'last_notified')
 
 
 def search():
